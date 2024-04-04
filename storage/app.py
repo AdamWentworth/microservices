bdcc745b946e7aef63f515018f53fed2958d77f0
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-04-04 15:39:08 -0700

    updated pool_recycle

--- storage/app.py
+++ storage/app.py
@@ -45,7 +45,7 @@ logger.info(f"Connecting to DB. Hostname:{db_host}, Port:{db_port}")
 engine = create_engine(
     f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}',
     pool_size=10,
-    pool_recycle=1800,
+    pool_recycle=300,
     pool_pre_ping=True
 )
 
